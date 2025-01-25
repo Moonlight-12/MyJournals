f4a576e275567eb16819836db85edad37da9df44
Geraldo-Unimelb <sadikingeraldo@gmail.com>
2025-01-25 20:00:09 +1100

    connected to mongoDB

--- server/route.js
+++ server/route.js
@@ -1,26 +1,39 @@
 const express = require("express");
-
 const router = express.Router();
+const { getConnectedClient } = require("./database");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("journaldb").collection("journal")
+    return collection
+}
 
 // GET /journal
-router.get("/journals", (req, res) => {
-  res.status(200).json({ mssg: "GET requres to /api/journals " });
+router.get("/journals", async (req, res) => {
+    const collection = getCollection();
+    const journal = await collection.find({}).toArray();
+
+    res.status(200).json(journal);
 });
 
 //POST /journal
-router.post("/journals", (req, res) => {
-    res.status(200).json({ mssg: "POST request to /api/journals"})
-})
+router.post("/journals", async (req, res) => {
+    const collection = getCollection();
+    // const { journal } = req.body;
+
+    console.log(req.body);
+
+    res.status(200).json({ mssg: "POST request to /api/journals" });
+});
 
 //DELETE /journal/:id
 router.delete("/journals/:id", (req, res) => {
-    res.status(200).json({ mssg: "DELETE request to /api/journals:id" })
-})
-
+  res.status(200).json({ mssg: "DELETE request to /api/journals:id" });
+});
 
 //PUT /journal/:id
 router.put("/journals/:id", (req, res) => {
-    res.status(200).json({ mssg: "PUT request to /api/journals:id" })
-})
+  res.status(200).json({ mssg: "PUT request to /api/journals:id" });
+});
 
 module.exports = router;
