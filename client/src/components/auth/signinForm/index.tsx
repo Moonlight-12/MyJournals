663220242692a7415f52823fdc2910a4cb703efa
Moonlight-12 <sadikingeraldo@gmail.com>
2025-03-10 13:59:07 +1100

    added assets

--- client/src/components/auth/signinForm/index.tsx
+++ client/src/components/auth/signinForm/index.tsx
@@ -61,7 +61,7 @@ export default function SigninForm() {
             <hr className="flex-grow"></hr>
           </div>
           <button
-            onClick={() => signIn("github", { callbackUrl: "/home", provider: "github" })}
+            onClick={() => signIn("github", { callbackUrl: "/next-home", provider: "github" })}
             className="w-full bg-gray-900 text-white py-2 rounded flex items-center justify-center"
           >
             <svg className="w-5 h-5 mr-2" fill="white" viewBox="0 0 24 24">
