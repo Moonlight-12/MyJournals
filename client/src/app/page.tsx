47cc19ab74db69b3a46a448a24394a9920e10545
Moonlight-12 <sadikingeraldo@gmail.com>
2025-06-06 17:30:17 +1000

    debugging

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
 import SignIn from "./(auth)/signin/page";
 
 export default function Home() {
+  console.log('Environment Variables Check:');
+console.log('NEXT_PUBLIC_APP_API_URL:', process.env.NEXT_PUBLIC_APP_API_URL);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
   return (
     
       <SignIn />
+      
     
   );
 }
